c6b407df8ec011da44e91a824ab6a1330b211168
SuperDaan0608 <daan09139@gmail.com>
2026-07-09 14:01:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Portfolio loaded successfully!");
+function scrollProjects(){
+
+document
+.getElementById("projects")
+.scrollIntoView();
+
+}
+
+
+
+
+const observer =
+new IntersectionObserver(entries=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+
+});
+
+
+});
+
+
+
+document
+.querySelectorAll(".hidden")
+.forEach(el=>observer.observe(el));
